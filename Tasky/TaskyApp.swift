c275e87312216cc60f33c419fdd976f001812faf
Mahdi BND <mahdi.bnd.7@gmail.com>
2023-06-01 13:40:21 +0330

    Initial Commit

---
+++ Tasky/TaskyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskyApp.swift
+//  Tasky
+//
+//  Created by Mahdi BND on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
